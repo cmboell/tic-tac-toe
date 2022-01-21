5564773feaaae86a13eccf5556479f8bc75be0c7
Colby Boell <68253967+cmboell@users.noreply.github.com>
2022-01-21 17:26:43 -0600

    minor fix
    minor fix

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,11 @@
 import React from 'react'
 //functional react component   //props is a parameter
 export default function Square(props){
+    return(
     //creates button           onClick to react to click 
     <button className="square" onClick={props.onClick}>
         {props.value} 
     </button>
     //props.value is caption of the button
+    )
 }
